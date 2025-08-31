3de2321967f506393be1d3d0a43402ec5670ccef
ABMBE <ermacsebas@gmail.com>
2025-08-31 00:30:23 -0500

    EJERCICIOS DE LA SEMANA 01

---
+++ S01_s01.01.js
@@ -0,0 +1,23 @@
+//PRACTICA S01_01
+//Declarar 3 variables de cada tipo e imprimir los valores
+
+//TIPO DE VARIABLE VAR
+var var1 = 4
+var var2 = 8
+var var3 = 7
+var miResultado = `Hola, somos variables definidas con VAR, soy variable1 = ${var1}, yo soy variable2 = ${var2}, yo soy variable3 = ${var3}`
+document.write('VARIABLES DEFINIDAS CON VAR' + "<br>" + miResultado + "<br>")
+
+//TIPO VARIABLE LET
+let var4 = 9
+let var5 = 10
+let var6 = 11
+let segResultado = `Ahora estamos definidas con let, yo soy variable4 = ${var4}, yo variable5 = ${var5}, yo variable6 = ${var6}`
+document.write('VARIABLES DEFINIDAS CON LET' + "<br>" + segResultado + "<br>")
+
+//TIPO VARIABLE CONST
+const var7 = 13
+const var8 = 14
+const var9 = 15
+let terResultado = `Ahora somo constantes, no podemos ser cambiadas, yo soy variable7 = ${var7}, yo soy variable8 = ${var8}, yo soy variable = ${var9}`
+document.write('VARIABLES DEFINIDAS COMO CONSTANTES' + "<br>" + terResultado + "<br>")
---
+++ S01_s01.02.js
@@ -0,0 +1,15 @@
+//tipo number
+let a = 5
+let b = 6
+document.getElementById("1-program").innerHTML = `Variable a = ${a}<br> Variable b = ${b}`
+
+//tipo string
+let miNombre = "Sebastian"
+let miApellido = "Medina"
+document.getElementById("2-program").innerHTML = `Hola, Mi nombres es: ${miNombre} y mi apellido es: ${miApellido}`
+
+//tipo boolean
+let isStudent = true
+let isTeacher = false
+document.getElementById("3-program").innerHTML = `Esta prueba muestra como funcionan los valores booleanos ¿Soy estudiante?
+${isStudent}, ¿Soy profesor?: ${isTeacher}`
